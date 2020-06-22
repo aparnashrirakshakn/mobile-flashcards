f57fc0d8f0faf633163659bbd621220afbd9228b
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-06-22 17:16:19 -0500

    add tabs to app

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import DeckList from './components/DeckList'
+import { FontAwesome, MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+import AddDeck from './components/AddDeck'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <DeckList />
-    </View>
-  );
+const Tab = createBottomTabNavigator();
+
+export default class App extends Component {
+  render() {
+    return(
+      <View style={{flex: 1}}>
+        <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Decks') {
+                iconName = focused
+                  ? 'cards'
+                  : 'cards-outline';
+              } else if (route.name === 'Add Deck') {
+                iconName = focused ? 'plus-box' : 'plus-outline';
+              }
+
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'blue',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Decks" component={DeckList} />
+          <Tab.Screen name="Add Deck" component={AddDeck} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
