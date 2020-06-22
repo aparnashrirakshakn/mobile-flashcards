03eed97f14d955c0d1a6781ddd9bb2946c09d772
aparnashrirakshakn <aparnashrirakshakn.me@gmail.com>
2020-06-22 18:29:15 -0500

    add functions to api file

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,5 @@
+import { AsyncStorage } from "react-native"
+
 const initialData = {
     React: {
         title: 'React',
@@ -48,4 +50,26 @@ const initialData = {
 
 export const getData = () => {
     return initialData
+}
+
+export function getDecks(deck) {
+    return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+            .then(results => {
+                if (results === null) {
+                    AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(initialData))
+                    return initialData
+                }
+                else {
+                    return JSON.parse(results)
+                }
+            })
+}
+
+export function saveDeckTitle() {
+    return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+        [title] : {
+            title: title,
+            questions: []
+        }
+    }))
 }
